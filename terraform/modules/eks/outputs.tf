6dc6ebcfc157f9374cf9154eca9cd17362c91f0e
EdsonJunior04 <edsonmacieljunior1000@gmail.com>
2026-09-15 01:11:00 -0300

    fix: cluster_security_group_id deve apontar para o SG que os nodes realmente usam

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -11,5 +11,8 @@ output "cluster_ca_certificate" {
 }
 
 output "cluster_security_group_id" {
-  value = aws_security_group.cluster.id
+  # O SG passado em vpc_config.security_group_ids so vale pro control plane.
+  # Os worker nodes (e portanto os Pods) usam o SG "cluster_security_group"
+  # que a propria AWS cria automaticamente - e esse que RDS/Redis precisam liberar.
+  value = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
 }
